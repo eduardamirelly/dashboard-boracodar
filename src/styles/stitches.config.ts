c0b668bfdea4e6b714d0dd68fe8e3ccaa98f2d41
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-03-06 20:34:49 -0300

    ➕ add stitches and create config

---
+++ src/styles/stitches.config.ts
@@ -0,0 +1,22 @@
+import { createStitches } from "@stitches/react";
+
+import { globalCss } from "@stitches/react";
+
+const globalStyles = globalCss({
+  '@import': ["url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap')"]
+});
+
+globalStyles();
+
+export const { css, styled, theme } = createStitches({
+  theme: {
+    fonts: {
+      primary: 'Inter, sans-serif',
+    },
+    colors: {
+      violetDark: '#292738',
+      violetMedium: '#363447',
+      white: '#ffffff',
+    },
+  },
+});
