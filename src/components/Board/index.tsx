04ccbbc078a733885d7ee649e28dfcfabb277b23
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-03-06 21:48:03 -0300

    💄 create Board

---
+++ src/components/Board/index.tsx
@@ -0,0 +1,9 @@
+import { BoardContainer } from "./styles";
+
+export function Board() {
+  return (
+    <BoardContainer>
+      Board
+    </BoardContainer>
+  )
+}
---
+++ src/components/Board/styles.ts
@@ -0,0 +1,10 @@
+import { styled } from "../../styles/stitches.config";
+
+export const BoardContainer = styled('div', {
+  backgroundColor: '$violetMedium',
+  borderRadius: '1rem',
+  boxShadow: '3px 4px 26px rgba(0, 0, 0, 0.25)',
+
+  paddingBlock: '3rem',
+  paddingInline: '2rem',
+});
